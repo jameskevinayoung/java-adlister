5424252bede183cbc2f8de87e87db1a4dca69fcf
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-10 10:49:09 -0500

    completed HelloWorldServlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,43 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+
+/**
+ * Step 1: Fork and clone the Adlister Repository
+ *
+ * Step 2: Checkout the servlets tag and branch off of it
+ *
+ * Step 3: Configure IntelliJ for the project
+ */
+
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    //step 5: create a counter to count page views
+    private int pageViewCount;
+
+    public void init() {
+        // Reset counter.
+        pageViewCount = 0;
+    }
+
+    //Step 4: Create a page that displays "Hello World!" when a user visits /hello
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+
+
+        res.setContentType("text/html");
+        //add the page view counter after setting the response type and in the out.println
+        pageViewCount++;
+
+        //Add "throws ServletException and IOException" to doGet() to remove .getWriter() error
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, World!</h1>\n" + "<h2> Page views: "+pageViewCount+"</h2>\n");
+
 
     }
 }
