cc1788b2094e4da5c23759afee0effae5ec5a04d
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-17 09:59:05 -0500

    finished jdbc

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,9 +1,20 @@
 public class DaoFactory {
+
+    /*
+    create a private static property that is an Ad object called adsDao;
+    this Data Access Object handles information retrieved from a database table and manipulates it for the model class
+     */
+
     private static Ads adsDao;
+
+
+    /*
+    create a static method that returns the adsDao; if the adsDao does not exist create one from the constructor
+    */
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao();
         }
         return adsDao;
     }
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -8,27 +8,35 @@ import java.util.List;
 
 public class MySQLAdsDao implements Ads{
 
-    //create private property of a connection object variable that will be initialized later in a constructor
-    //method
+    /*
+    create private property of a connection object variable that will be initialized later in a constructor
+    method
+    */
 
     private Connection connection;
 
     //create private property of a list type of ads that will be initialized in the methods implemented from Ads Interface
     private List<Ad> ads;
 
-    //create a new configuration object that will use the Config class that contains the url, username, and password
-    //methods that create a connection. This would be used if you didn't have Config config in the parameter portion
-    //of the MYSQLAdsDao constructor
-//    Config config = new Config();
+    /*
+    create a new configuration object that will use the Config class that contains the url, username, and password
+    methods that create a connection. If you placed parameters in the MySQLAdsDao you would have to create another
+    object later in the Dao Factory instead of doing it this way
+    */
+    Config config = new Config();
 
 
-    //create a constructor that accepts a Config parameter and obtains database information
-    //this requires the ability to use the driver manager to retrieve the connection, and handle any exception errors
+    /*
+    create a constructor that accepts a Config parameter and obtains database information
+    this requires the ability to use the driver manager to retrieve the connection, and handle any exception errors
+    */
 
-    public MySQLAdsDao(Config config) {
+    public MySQLAdsDao() {
 
-    //This DriverManager.getConnection() has an unhandled exception to take into account
-    //handle the exception with a try/catch method
+    /*
+    This DriverManager.getConnection() has an unhandled exception to take into account
+    handle the exception with a try/catch method
+    */
 
         try {
             connection = DriverManager.getConnection(
@@ -39,23 +47,31 @@ public class MySQLAdsDao implements Ads{
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        //The connection object will be created just once, in this class' constructor, and the individual methods
-        //that query the database should use the connection object to create statements.
+        /*
+        The connection object will be created just once, in this class' constructor, and the individual methods
+        that query the database should use the connection object to create statements.
+        */
     }
 
 
 //Implementing the methods from Ads interface
 
-    //in order to list all the ads create a method that retrieves information from the database
-
+    /*in order to list all the ads create a method that retrieves information from the database and make sure this overrides
+    any other similar all methods for the list of ads if they exist
+    */
+    @Override
     public List<Ad> all(){
-        //create a String variable that holds the select statement for the table from where data is being retrieved;
-        //this db statement is retrieving all of the items available in the database
+        /*
+        create a String variable that holds the select statement for the table from where data is being retrieved;
+        this db statement is retrieving all of the items available in the database
+        */
 
         String reviewQuery = "Select * FROM ads";
 
-        //create a List object that is a new Array list
-        //this will take each row in the database and input that in each index of the array list
+        /*
+        create a List object that is a new Array list
+        this will take each row in the database and input that in each index of the array list
+        */
 
         List<Ad> ads = new ArrayList<>();
 
@@ -63,8 +79,10 @@ public class MySQLAdsDao implements Ads{
 
         try {
 
-        //assign the connection property and the createStatement method to the statement;
-        //create a result set object and assign the statement with executeQuery method;
+        /*
+        assign the connection property and the createStatement method to the statement;
+        create a result set object and assign the statement with executeQuery method;
+        */
 
             Statement statement =  connection.createStatement();
 
@@ -72,9 +90,11 @@ public class MySQLAdsDao implements Ads{
 
             ResultSet results = statement.executeQuery(reviewQuery);
 
-         //iterate over the set of results with the .next() method in a while loop
-         //then add each ad in the results; transfer information from the database to the java language by using a constructor
-         //the add method needs a receive an Ad object
+         /*
+         iterate over the set of results with the .next() method in a while loop
+         then add each ad in the results; transfer information from the database to the java language by using a constructor
+         the add method needs to receive an Ad object; since it doesn't exist yet, create an Ad object method to do so
+         */
 
             while(results.next()){
                 ads.add(createAd(results));
@@ -87,8 +107,8 @@ public class MySQLAdsDao implements Ads{
     }
 
     //this is an Ad generator that is creating an Ad object  from a set of results from a database;
-        //to create an ad with a return type as an Ad object
-        //the constructor takes a ResultSet parameter
+
+    //to create an ad with a return type as an Ad object, the constructor takes a ResultSet parameter
 
     public Ad createAd(ResultSet results){
 
@@ -96,9 +116,10 @@ public class MySQLAdsDao implements Ads{
 
         try{
 
-         //return a new ad from the results
-         //the ad would contain the id column, user_id column, title column, and description column
-         //this translates the information from the database to a java object
+         /*
+         return a new ad from the results the ad would contain the id column, user_id column, title column, and description column
+         this translates the information from the database to a java object
+         */
 
             return new Ad (results.getLong("id"),
                     results.getLong("user_id"),
@@ -115,20 +136,52 @@ public class MySQLAdsDao implements Ads{
 
     // create an insert method that takes the ad object and returns a long integer;
     // because this is a different
+
+    @Override
     public Long insert(Ad ad){
-        //create a string object for the query statement and format the string object with the title and description
-        //use the mysql insert statement in parenthesis with the values as  wildcard variables
-        //use the getTitle and getDescription methods on the ad to properly insert the data in the database
+
+
+        /*
+        create a string object for the query statement and format the string object with the title and description
+        use the mysql insert statement in parenthesis with the values as  wildcard variables
+        use the getTitle and getDescription methods on the ad to properly insert the data in the database
+        */
+
+
         String insertQuery = String.format("INSERT INTO ads(title,description) values ('%s', '%s')", ad.getTitle(), ad.getDescription());
 
-        //use try/catch block with the
+
+        //use try/catch block with the statement variable holding the connection to the database to retrieve the information
         try{
+            Statement statement = connection.createStatement();
+
+
+        //use the statement variable to execute the insertQuery to update the database along with providing access to
+        //the keys that are generated, which is the automatically generated id of the new row to the database
+
+            statement.executeUpdate(insertQuery,Statement.RETURN_GENERATED_KEYS);
+
+        //assign the updated insert statement to the result set and use the getGeneratedKeys method to return a result
+        //that holds these keys
+
+        ResultSet result =  statement.getGeneratedKeys();
+
+        //create a conditional statement that uses the next method to move to the next result and returns the result id
+        //which is the first column, aka the id column
+
+        if(result.next()){
+
+            //use the getLong(columnLabel) retrieves the column title of the result that we want, which is the id
+
+            return result.getLong("id");
+        }
 
         }catch(SQLException e){
             e.printStackTrace();
         }
-
         return null;
     }
 
+
 }
+//THIS DAO WILL REPLACE THE ListAdsDao FILE IN THE DAO FACTORY
