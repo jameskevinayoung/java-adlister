08151c386138302a976b64f7bb7d6f9333a3da79
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-18 15:15:37 -0500

    updated part 1 with notes

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -9,8 +9,23 @@ public class User {
     private String username;
     private String email;
     private String password;
+//---------- CREATES AN EMPTY USER ----------//
 
     public User() {}
+
+
+//---------- CREATES A USER OBJECT WITH A USERNAME, EMAIL, AND PASSWORD ----------//
+
+    /** USE THE SETPASSWORD METHOD
+     * When creating a User object replace the instance of the password (this.password) with setPassword method
+     * that takes in the password parameter. This is because the setPassword method already hashes the instance
+     * of password when the User class is created. ex( this.password = hashPassword(password) )
+     * Using setPassword method reduces redundancy in code and automatically takes care of the hashing process.
+     * When we use the getParameter method in MySQLUsersDao, we are using this constructor thereby retrieving a hashed
+     * password and providing security on password credentials for our users preventing accidental data breaches.
+     *
+     */
+
 
     public User(String username, String email, String password) {
         this.username = username;
@@ -18,11 +33,20 @@ public class User {
         setPassword(password);
     }
 
+//---------- CREATES A USER WITH AN ID, USERNAME, EMAIL, AND PASSWORD ----------//
+
+    /** DO NOT USE THE SETPASSWORD METHOD
+     * When using this constructor if we used setPassword method it would double hash the password and create
+     * unintentional consequences that would become more difficult as the apps complexity increases.
+     *
+     */
+
+
     public User(long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
-        setPassword(password);
+        this.password = password;
     }
 
     public long getId() {
@@ -54,10 +78,22 @@ public class User {
     }
 //-----------------This is to hash a password and set it before reaching the MySQLDao------------//
 
+    /** IN ORDER TO PROPERLY HASH THE PASSWORD YOU HAVE TO SET THE PASSWORD TO A USER OBJECT
+     * 1)Use the setPassword method that takes a password string
+     * 2)Use the hashPassword function that takes the password parameter and assigns it to THIS instance of the password
+     */
 
     public void setPassword(String password) {
         this.password = hashPassword(password);
     }
+
+
+/**
+ * 1)Create a method to hash passwords by taking a string variable and return a String variable of the hashed password
+ * 2)Get the password for THIS instance of the User class and assign it to a String variable (userpassword) to hold the password
+ * 3)Use the BCrypt.gensalt method and the password as parameters inside the BCrypt.hashpw method to hash the password
+ * 4)Assign the BCrypt.hashpw method to as String variable that will hold the hashed password and return that String variable
+ */
 
     public String hashPassword(String password) {
         String userPassword = this.getPassword();
